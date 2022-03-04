47b36a4ae4bbd5ee4f920e34bfc3f1b40eeca154
Kevin Wainwright <mr_rich199@hotmail.com>
2022-03-04 11:43:14 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UsingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("C# is Cool!");
+            Console.Read();
+        }
+    }
+}
